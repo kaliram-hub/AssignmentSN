a242ada4c9d7fe4d7308e2cab9a05cd7cad39e51
kaliram-hub <59826451+kaliram-hub@users.noreply.github.com>
2022-09-14 17:50:52 +0530

    first commit
    I have added the source file for the assignment answer.
    you can also visit the leetcode site where I submitted the answer for the same question.
    
    LeetCode ID : krdm   
    url: https://leetcode.com/krdm/
    GeeksForGeeks profile: krdmahapatro

---
+++ LeaderTest.java
@@ -0,0 +1,38 @@
+package com.assignment.leader;
+
+import java.util.Collections;
+
+public class LeaderTest {
+
+	 /* Java Function to print leaders in an array */
+    void printLeaders(int arr[], int size)
+    {
+        int max_from_right =  arr[size-1];
+   
+        /* Rightmost element is always leader */
+        System.out.print(max_from_right + " ");
+       
+        for (int i = size-2; i >= 0; i--)
+        {
+            if (max_from_right < arr[i])
+            {           
+            max_from_right = arr[i];
+            System.out.print(max_from_right + " ");
+            }
+        }    
+    }
+  
+    /* Driver program to test above functions */
+    public static void main(String[] args) 
+    {
+    	LeaderTest lead = new LeaderTest();
+        int arr[] = new int[]{16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+        
+        lead.printLeaders(arr, n);
+    }
+
+}
+
+
+
